9f34728462f6c7c60244ab0ed795af2eb4f868b8
Gette <upwyy123@gmail.com>
2021-03-18 15:59:08 +0900

    1.JPA를 이용하여 관계형 데이터베이스를 객체지향적으로 관리하는방법 2.JPA의 더티 체킹을 이용하면 Update문 쿼리 없이 테이블 수정이 가능 3.JPA Auditing을 이용하여 등록/수정 시간을 자동화하는 방법

--- src/test/java/com/jojoldu/book/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,5 +43,30 @@ public void 게시글저장_불러오기(){
     Posts posts = postsList.get(0);
     assertThat(posts.getTitle()).isEqualTo(title);
     assertThat(posts.getContent()).isEqualTo(content);
+}
+
+
+@Test
+public  void BaseTimeEntity_등록(){
+        //given
+    LocalDateTime now = LocalDateTime.of(2019,6,4,1,0,0);
+    postsRepository.save(Posts.builder()
+    .title("title")
+    .content("content")
+    .author("autyhor")
+    .build());
+
+    //when
+    List<Posts> postsList = postsRepository.findAll();
+
+    //then
+    Posts posts = postsList.get(0);
+
+    System.out.println(">>>>>> createDate= "+posts.getCreatedDate()+", modifiedDate= "+posts.getModifiedDate());;
+
+
+    assertThat(posts.getCreatedDate()).isAfter(now);
+    assertThat(posts.getModifiedDate()).isAfter(now);
+
 }
 }
